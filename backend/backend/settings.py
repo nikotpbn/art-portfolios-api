788d531d009c8b030a855c71b921c3f1de00def9
Nicolau Tolentino Pamplona Beltrão Neto <niko@nicolaus-mbp.lan>
2023-03-08 16:18:44 +0000

    fixing test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,9 +92,8 @@ DATABASES = {
         'ENGINE'  : 'django.db.backends.mysql',
         'NAME'    : os.environ.get('DB_NAME'),
         'USER'    : os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST'    : os.environ.get('DB_HOST'),
-        'PORT'    : os.environ.get('DB_PORT'),
     }
 }
 
